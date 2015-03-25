de0762fa6a49a2d9e3d6bae0c00de453b6d4b4ac
YoncaCoskun <yoncacoskun92@gmail.com>
2015-03-25 21:22:24 +0200

    Scripts  and Examples

---
+++ 23 MartJqueryBaslangic/JavaScript.js
@@ -0,0 +1,16 @@
+﻿function $(metin) {
+    return new Nesne(document.getElementById(metin));
+
+
+}
+
+function Nesne(HtmlNesnesi) {
+    this.html = function (yazi) {
+        HtmlNesnesi.innerHTML = yazi;  // yazıyı olusturma
+    }
+    this.bgColor = function (color1, color2) {
+        HtmlNesnesi.style.backgroundColor = color1; //arkaplan rengı olusturma
+        HtmlNesnesi.style.color = color2;
+    }
+
+}
