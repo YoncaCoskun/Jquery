3232210a760efe478e16aa9be5580f5128c0d8d7
YoncaCoskun <yoncacoskun92@gmail.com>
2015-03-28 22:36:40 +0200

    Jquery

---
+++ 26Mart/JQueryAjax/VeriCekme.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="VeriCekme.aspx.cs" Inherits="_Default" %>
+
---
+++ 26Mart/JQueryAjax/VeriCekme.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        var isim=Request["isim"];
+        var soyisim = Request["soyisim"];
+        Response.Write("Merhaba " +isim+" "+soyisim);  //aspx?isim=yonca&&soyisim=coşkun yaptıgımızda merhaba yonca coşkun gelir
+    }
+}
